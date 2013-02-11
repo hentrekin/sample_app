83ddf8def260a40e33b81c759b9be4da9fe32218
Hawkins Entrekin <hawkins@Forge.(none)>
2013-02-11 13:01:37 -0600

    10.1.4

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,6 +26,7 @@ require 'spec_helper'
     it { should respond_to(:remember_token) }
     it { should respond_to(:authenticate) }
     it { should respond_to(:admin) }
+    it { should respond_to(:microposts) }
 
     it { should be_valid }
     it { should_not be_admin }
